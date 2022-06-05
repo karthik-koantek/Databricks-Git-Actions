28266e23784c3978aeb62b5e5f9b884dad9d3a92
karthik-koantek <106896843+karthik-koantek@users.noreply.github.com>
2022-06-05 10:06:42 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ variable "username" {
 }
 
 variable "password" {
-  description = "The code of the AWS Region to use."
+  description = "The code of the password to use."
   type = string
   default = ""
 }
